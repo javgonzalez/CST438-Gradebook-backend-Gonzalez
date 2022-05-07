e8d7a61733d5ec768ff2e4706199cd60fd104b98
javgonzalez <javgonzalez@csumb.edu>
2022-05-07 13:55:01 -0700

    Test Div Commit

--- cst438_gradebook/src/main/java/com/cst438/Cst438GradebookApplication.java
+++ cst438_gradebook/src/main/java/com/cst438/Cst438GradebookApplication.java
@@ -15,6 +15,7 @@ public class Cst438GradebookApplication  {
 	public static void main(String[] args) throws InterruptedException {
 		// delay for 1 minute to allow time for Docker compose to start database service.
 		// Thread.sleep(60000); 
+	   // Testing Dev Commit!
 		SpringApplication.run(Cst438GradebookApplication.class, args);
 	}
 	
